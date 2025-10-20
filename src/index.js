1757f38eef8386f679967bd9deca2c80aae726ed
Akhilesh Lakhanpal <akhileshlakhanpal660@gmail.com>
2025-10-20 16:28:38 +0530

    New COmmit

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 // --- Middleware ---
 app.use(cors({
     // Allow the client (Next.js) to access the API
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true
 }));
 
